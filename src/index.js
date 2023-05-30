f70034b37fdfffc357ca66efdcb64f0012e4e9ef
Pamir Perdeci <pamirperdeci@gmail.com>
2023-05-30 09:05:03 -0400

    state connected to multiple components via redux and rendering movie data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import reducer from './reducers';
@@ -9,9 +9,15 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+const store = createStore(
+  reducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
 ReactDOM.render(
+  <Provider store={store}>
   <Router>
-      <App />
-  </Router>,
+  <App />
+  </Router>
+  </Provider>,
   document.getElementById('root')
 );
